ba3c864373f3cddc69a33c9b6aead9a52b090e71
Honza Novak <kacerr.cz@gmail.com>
2015-02-13 00:22:48 +0100

    Do not store Db credentials in database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/srv/webs/stukdo'
 
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-set :linked_files, fetch(:linked_files, []).push('config/.env.production')
+set :linked_files, fetch(:linked_files, []).push('.env.production')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
